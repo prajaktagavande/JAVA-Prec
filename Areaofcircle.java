153ba8ccd64635c822584b3915ac3d89fa026eb8
prajaktagavande <142582747+prajaktagavande@users.noreply.github.com>
2023-08-28 17:06:14 +0530

    Add files via upload

---
+++ Areaofcircle.java
@@ -0,0 +1,11 @@
+//Write a Java program to print the area of a circle//
+ class AreaOfCircle{
+public static void main(String args[])
+{
+int r=6;
+double pi=3.14;
+double area;
+area =pi*r*r;
+System.out.println("area of circle: "+area);
+}
+}
