c4ff84017d37a0636d7c27410bc0b1f57f9b4f7f
prajaktagavande <142582747+prajaktagavande@users.noreply.github.com>
2023-08-28 17:07:01 +0530

    Add files via upload

---
+++ Calculation.java
@@ -0,0 +1,18 @@
+//Q2.Write a Java program to print the sum (addition), multiply, subtract, divide, and remainder of two numbers.//
+class Calculations{
+public static void main(String args[])
+{
+int a=2,b=3;
+int s1=a+b;
+int s2=a-b;
+int s3=a/b;
+int s4=a*b;
+int s5=a%b;
+System.out.println(s1);
+System.out.println(s2);
+System.out.println(s3);
+System.out.println(s4);
+System.out.println(s5);
+
+}
+}
